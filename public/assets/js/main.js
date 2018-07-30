31986f93781a7468e82474900096bad231616ba6
LeslieInostroza <leslie.ino@gmail.com>
2018-07-30 18:15:32 -0400

    ordenando contenido

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,29 +1,16 @@
- document.addEventListener('DOMContentLoaded', function() {
-    try {
-      let app = firebase.app();
-      var uiConfig = {
-    signInSuccessUrl: '<url-to-redirect-to-on-success>',  // que vuelva al home ( pagina de menubotones)
-    signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    ],
-    // Terms of service url.
-    tosUrl: '<your-tos-url>'// ruta home ( terminos y condiciones)
-
-  };
+document.addEventListener('DOMContentLoaded', function() {
+  try {
+    let app = firebase.app();
+    
   firebase.auth().onAuthStateChanged((user)=> {
-      if (user) {
-        console.log(user);
-        document.getElementById('userData').innerHTML='Hola' + user.displayName +'<a href="#" onClick="firebase.auth().signOut()">Cerrar Sesion</a>';
-      }else{
-        document.getElementById('userData').innerHTML='';// para limpiar al deslogearse
-              // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
-      }
-    });
-  } catch (e) {
-      console.error(e);
+    if (user) {
+      console.log(user);
+      document.getElementById('userData').innerHTML='Hola' + user.displayName + '<a href="#" onClick="firebase.auth().signOut()">Cerrar Sesion</a>';
+    }else{
+      document.getElementById('userData').innerHTML='';// para limpiar al deslogearse
     }
   });
+} catch (e) {
+  console.error(e);
+}
+});
--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,1 +1,18 @@
 //iniciar sesión para 3 tipos de usuarios recepción, administación y oficinas
+const login = () => {
+  var uiConfig = {
+    // que vuelva al home (pagina de menubotones)
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+   ],
+   // Terms of service url.
+   tosUrl: '<your-tos-url>'// ruta home ( terminos y condiciones)
+  };
+  
+      // Initialize the FirebaseUI Widget using Firebase.
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      // The start method will wait until the DOM is loaded.
+      ui.start('#firebaseui-auth-container', uiConfig);
+}
