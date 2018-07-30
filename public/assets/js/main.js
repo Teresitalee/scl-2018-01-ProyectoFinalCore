4cc75e2bd3037d1541ac700eda928388592ed958
TeresitaLee <teresita.1852@hotmail.com>
2018-07-30 17:33:47 -0400

    agregando firebase

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,29 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBDzDQCOH_4xeG6OPUJcIRXPzbIIVHzlvs",
-    authDomain: "registro-de-usuario-d5489.firebaseapp.com",
-    databaseURL: "https://registro-de-usuario-d5489.firebaseio.com",
-    projectId: "registro-de-usuario-d5489",
-    storageBucket: "registro-de-usuario-d5489.appspot.com",
-    messagingSenderId: "396909058160"
-  };
-  firebase.initializeApp(config);
+document.addEventListener('DOMContentLoaded', function() {
+  try {
+    let app = firebase.app();
+    var uiConfig = {
+  signInSuccessUrl: '<url-to-redirect-to-on-success>',  // que vuelva al home ( pagina de menubotones)
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>'// ruta home ( terminos y condiciones)
+
+};
+firebase.auth().onAuthStateChanged((user)=> {
+    if (user) {
+      console.log(user);
+      document.getElementById('userData').innerHTML='Hola' + user.displayName +'<a href="#" onClick="firebase.auth().signOut()">Cerrar Sesion</a>';
+    }else{
+      document.getElementById('userData').innerHTML='';// para limpiar al deslogearse
+            // Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+    }
+  });
+} catch (e) {
+    console.error(e);
+  }
+});
