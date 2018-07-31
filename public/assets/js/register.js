96232ea82ef5c722a4f6e103598fedc59914a008
PaulaAraya <paula.arayawilliams@gmail.com>
2018-07-31 11:00:11 -0300

    ruta de registro de visitas

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -1,1 +1,27 @@
 //regista las visitas
+const name = name.value;
+const lastName = lastName.value;
+const rut = rut.value;
+
+window.register = ()=>{
+  //variable con ruta agregar nuevo registro
+  let newRegister = firebase
+  .database()
+  .ref()
+  .child("register")
+  .push().key;
+  //Aquí se muestra el contenido de cada registro
+  firebase
+  .database()
+  .ref(`register/${newRegister} `)
+  .set({
+    name: name,
+    lastName: lastName,
+    rut: rut
+  });
+}
+
+// En esta constante se imprimen los nuevos registro en html
+const printRegister = register =>{
+
+}
