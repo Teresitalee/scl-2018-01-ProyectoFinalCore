ce676e7a518d5474c28cd3781af02814a0adfbfa
LeslieInostroza <leslie.ino@gmail.com>
2018-07-31 08:29:13 -0400

    pequeños arreglos

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,25 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
   try {
     let app = firebase.app();
-    
-  firebase.auth().onAuthStateChanged((user)=> {
-    if (user) {
-      console.log(user);
-      hideAll();
-      document.getElementById('menuPage').style.display = 'block';
-      document.getElementById('userData').innerHTML='Bienvenid@ ' + user.displayName + ' <a href="#" onClick="signOut()">Cerrar Sesion</a>';
-
-    }else{
-      hideAll();
-      document.getElementById('menuPage').style.display = 'none'
-      document.getElementById('userData').innerHTML='';// para limpiar al deslogearse
-      login();
-    }
-  });
-} catch (e) {
-  console.error(e);
-}
-
+    firebase.auth().onAuthStateChanged((user)=> {
+      if (user) {
+        console.log(user);
+        hideAll();
+        document.getElementById('menuPage').style.display = 'block';
+        document.getElementById('userData').innerHTML='Bienvenid@ ' + user.displayName + ' <a href="#" onClick="signOut()">Cerrar Sesion</a>';
+      }else{
+        hideAll();
+        document.getElementById('menuPage').style.display = 'none'
+        document.getElementById('userData').innerHTML='';// para limpiar al deslogearse
+        login();
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
+  
   addListenerToClass('btnrecepcion', showRegister);
   addListenerToClass('btnoficina', showAdmission);
   addListenerToClass('btnadministracion', showAdministrador);
--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -1,20 +1,17 @@
 //iniciar sesión para 3 tipos de usuarios recepción, administación y oficinas
 const login = () => {
-  var uiConfig = {
+  let uiConfig = {
     // que vuelva al home (pagina de menubotones)
     signInSuccessUrl: '<url-to-redirect-to-on-success>',
     signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-   ],
-   // Terms of service url.
-   tosUrl: '<your-tos-url>'// ruta home ( terminos y condiciones)
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ],
+    tosUrl: '<your-tos-url>'// ruta home ( terminos y condiciones)Terms of service url.
   };
-  
-      // Initialize the FirebaseUI Widget using Firebase.
-      var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      // The start method will wait until the DOM is loaded.
-      ui.start('#firebaseui-auth-container', uiConfig);
+  // Initialize the FirebaseUI Widget using Firebase.
+  let ui = new firebaseui.auth.AuthUI(firebase.auth());
+  // The start method will wait until the DOM is loaded.
+  ui.start('#firebaseui-auth-container', uiConfig);
 }
 
 const signOut = () => {
